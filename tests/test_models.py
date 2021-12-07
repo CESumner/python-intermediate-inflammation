f186159d9150b37d3e74662a6018714037d69b9b
CESumner <chs73@aber.ac.uk>
2021-12-07 11:31:37 +0000

    Added data validation and extended unit testing for data normalisation

--- tests/test_models.py
+++ tests/test_models.py
@@ -43,9 +43,48 @@ def test_daily_min(test, expected):
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 
+def test_daily_max_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_max
+
+    with pytest.raises(TypeError):
+        error_expected = daily_max([['Hello', 'there'], ['General', 'Kenobi']])
+
+
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+
+@pytest.mark.parametrize(
+    "test, expected, raises",
+    [
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
+    ])
+def test_patient_normalise(test, expected, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if isinstance(test, list):
+        test = np.array(test)
+    if raises:
+        with pytest.raises(raises):
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
