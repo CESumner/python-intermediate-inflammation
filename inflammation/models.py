6c5dfde802f710fa64844a6bcbf1b79fdfc48419
CESumner <chs73@aber.ac.uk>
2021-12-08 11:23:09 +0000

    added class structure to models and added tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -1,73 +1,68 @@
-"""Module containing models representing patients and their data.
+class Observation:
+    def __init__(self, value, day):
+        self.day = day
+        self.value = value
 
-The Model layer is responsible for the 'business logic' part of the software.
+    def __str__(self):
+        return str(self.value)
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
-and each column represents a single day across all patients.
-"""
+class Person:
+    def __init__(self, name):
+        self.name = name
 
-import numpy as np
+    def __str__(self):
+        return self.name
 
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.trial_group = None
+        self.doctor = None
+        self.observations = []
 
-def load_csv(filename):
-    """Load a Numpy array from a CSV
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
 
-    :param filename: Filename of CSV to load
-    """
-    return np.loadtxt(fname=filename, delimiter=',')
+            except IndexError:
+                day = 0
 
+        new_observation = Observation(value, day)
 
-def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array for each day.
-
-   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-   :returns: An array of mean values of measurements for each day.
-   """
-    return np.mean(data, axis=0)
-
-
-def daily_max(data):
-    """Calculate the daily maximum of a 2D inflammation data array for each day.
-
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-    :returns: An array of max values of measurements for each day.
-    """
-    return np.max(data, axis=0)
-
-
-def daily_min(data):
-    """Calculate the daily minimum of a 2D inflammation data array for each day.
-
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
-    :returns: An array of minimum values of measurements for each day.
-    """
-    return np.min(data, axis=0)
+        self.observations.append(new_observation)
+        return new_observation
 
 
-def patient_normalise(data):
-    """
-    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
 
-    Any NaN values are ignored, and normalised to 0
+    def add_patient(self, Patient):
+        self.patients.append(Patient)
+        Patient.doctor = self.name
 
-    :param data: 2d array of inflammation data
-    :type data: ndarray
+    @property
+    def patient_list(self):
+        return self.patients
 
-    """
-    if not isinstance(data, np.ndarray):
-        raise TypeError('data input should be ndarray')
-    if len(data.shape) != 2:
-        raise ValueError('inflammation array should be 2-dimensional')
-    if np.any(data < 0):
-        raise ValueError('inflammation values should be non-negative')
-    max = np.nanmax(data, axis=1)
-    with np.errstate(invalid='ignore', divide='ignore'):
-        normalised = data / max[:, np.newaxis]
-    normalised[np.isnan(normalised)] = 0
-    return normalised
+    def __str__(self):
+        return self.name
 
+alice = Patient('Alice')
+print(alice)
 
-# TODO(lesson-design) Add Patient class
-# TODO(lesson-design) Implement data persistence
-# TODO(lesson-design) Add Doctor class
+obs = alice.add_observation(3)
+print(obs)
+
+bob = Person('Bob')
+print(bob)
+
+DrSmith = Doctor("Dr Smith")
+DrSmith.add_patient((alice))
+patients = DrSmith.patient_list
+for patient in patients:
+    print(patient)
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -5,6 +5,21 @@ def test_create_patient():
     from inflammation.models import Patient
 
     name = 'Alice'
+    obs = 'test obs'
     p = Patient(name=name)
+    p.add_observation(obs)
 
     assert p.name == name
+    assert p.observations == obs
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Dr Smith'
+    patients = ['Alice', 'Beth', 'Chloe']
+    d = Doctor(name=name)
+    for patient in patients:
+        d.add_patient(patient)
+
+    assert d.name == name
+    assert d.patients == patients
